de27c9fceeca71933cc946346c6cc3ec6d5f46e0
Niramay Thaker <thakerniramay@gmail.com>
2023-02-06 19:18:25 +0530

    Featching data from url

--- mynotes_frontend/src/pages/NotePage.js
+++ mynotes_frontend/src/pages/NotePage.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 
-const NotePage = ({match}) => {
-    // const noteId = match.params.id 
-    const { id } = useParams();
+
+// const NotePage = ({match}) => {
+//     let { id } = useParams();
+
+//     return (
+    //         <div>
+    //             <p>Single Note: {id}</p>
+    //         </div>
+    //     )
+    // }
+    
+    
+const NotePage = () => {
+    let { id } = useParams();
+    let [note, stateNotes] = useState(null)
+    
+    useEffect(()=> {
+        getNotes()
+    }, [])
+    
+    let getNotes = async () => {
+        let response = await fetch(`/api/notes/${id}`)
+        let data = await response.json()
+        console.log("DATA: ", data)
+        stateNotes(data)
+        note = data
+    } 
 
     return (
-        <div>Single Note {id}</div>
+        <div>
+        <div>
+            <p>
+                {note?.body}
+            </p>  
+        </div>
+    </div>
     )
 }
 
